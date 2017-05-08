67d6dcaf3bac5d4b16fc6a3259f592b2ad228beb
Arturo Garcia <arturo.garcia@nucleodf.com>
2017-05-08 11:05:07 +0200

    20170508. Modificaciones en la ventana de Version
    Signed-off-by: Arturo Garcia <arturo.garcia@nucleodf.com>

--- public/javascripts/version.js
+++ public/javascripts/version.js
@@ -1,6 +1,6 @@
 
 var GetVersion = function(isFirstLoad) {
-	$('#DivVersion').animate({width: '535px'});
+	$('#DivVersion').animate({width: '435px'});
 	if(!isFirstLoad) {
 		$("#AddFormVersion").show();
 		$("#FormVersion").show();
@@ -8,7 +8,9 @@ var GetVersion = function(isFirstLoad) {
 	$.ajax({type: 'GET',
 			url: '/version'})
 		.done(function(data){
-			$("#IdVersion").text(data.version);
+			// $("#IdVersion").text(data.version);
+			var strSubversion = data.subversion=='' ? ',  ' : ('.' + data.subversion + ',  ');
+			$("#IdVersion").text(data.version + strSubversion + data.date  );
 			$("#IdSubVersion").text(data.subversion);
 			$("#IdVersionDate").text(data.date);
 			$("#IdNodeJSVersion").text(data.nodejsversion);
@@ -17,9 +19,15 @@ var GetVersion = function(isFirstLoad) {
 			$("#hVersion").text(data.version+' '+data.subversion);
 			
 			$.each(data.file, function(index, value){
-				var item = $("<br>Fichero: <b>"+value.Name+"</b><ul><li>MD5: "+value.md5.toUpperCase()+"</li>" +
-					"<li>Tamaño: "+value.fileSizeInBytes+" bytes</li><li>Fecha: "+value.date+"</li></ul>");
-				
+				// var item = $("<br>Fichero: <b>"+value.Name+"</b><ul><li>MD5: "+value.md5.toUpperCase()+"</li>" +
+					// "<li>Tamaño: "+value.fileSizeInBytes+" bytes</li><li>Fecha: "+value.date+"</li></ul>");
+				var item = $("<p style='color:black';>" +
+							 "<b>" + value.Name + "</b>" +
+							 ", "  + value.date +
+							 ", "  + value.fileSizeInBytes + ' bytes' +
+//							 ", "  + value.md5.toUpperCase() +
+							 "</p>"
+							);
 				item.appendTo($("#listVersions"));
 			});
 		});
